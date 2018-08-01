f03bea0ac16ce51559e4442bab05ee65eadc1349
dusan991 <papovic.pes@gmail.com>
2018-08-01 22:26:17 +0200

    prvi

---
+++ src/main/java/com/iktpreobuka/elektronskidnevnik/repositories/OcenaRepository.java
@@ -0,0 +1,17 @@
+package com.iktpreobuka.elektronskidnevnik.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.iktpreobuka.elektronskidnevnik.entities.OcenaEntity;
+import com.iktpreobuka.elektronskidnevnik.entities.OdeljenjePredmetNastavnikEntity;
+import com.iktpreobuka.elektronskidnevnik.entities.UcenikEntity;
+
+public interface OcenaRepository extends CrudRepository<OcenaEntity, Integer> {
+
+	List<OcenaEntity> findByUcenik(UcenikEntity ucenik);
+
+	List<OcenaEntity> findByOdeljenjePredmetNastavnik(OdeljenjePredmetNastavnikEntity nekiPredmet);
+
+}
